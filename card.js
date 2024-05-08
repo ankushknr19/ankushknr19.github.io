a15fc24a2b390215931b3197fc7f334e6d273945
Ankush Kunwar <ankush.kunwar@amniltech.com>
2024-05-08 14:56:24 +0545

    update: 2024

--- card.js
+++
@@ -1,8 +0,0 @@
-const toggleThemeButton = document.getElementById('theme-toggle-checkbox')
-
-toggleThemeButton.addEventListener('change', themeToggleHandler)
-
-function themeToggleHandler() {
-	document.body.classList.toggle('light')
-	document.getElementById('profile-card').classList.toggle('light')
-}
